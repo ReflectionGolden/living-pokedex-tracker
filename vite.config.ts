2f495b1f4dec8cb7fdc01eb1be44cc80a13476b1
Darrenvandervelde <Darrenvandervelde@gmail.com>
2026-07-03 16:31:42 +0100

    chore: add vite-plugin-node-polyfills to support Node.js globals in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import netlify from "@netlify/vite-plugin";
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    nodePolyfills({
+      // To exclude the unnescessary polyfills, specify which ones to include
+      include: ['buffer'],
+      // Whether to polyfill Globals like 'buffer' and 'process'
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+      }
+    }),
     netlify()
   ],
 })
